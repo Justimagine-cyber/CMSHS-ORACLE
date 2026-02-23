6e974bc703647237453ed651795317fbd850d94f
Justimagine-cyber <83823401+Justimagine-cyber@users.noreply.github.com>
2026-02-23 23:30:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,8 @@
-const CACHE_NAME = 'oracle-cmshs-v1';
 const assets = [
   './',
   './index.html',
   './styles.css',
   './script.js',
   './CMSHS_LOGO.png',
-  './CMSHS SDRRM PLAN.png'
+  './CMSHS SDRRM PLAN.png',
 ];
-
-// Install Service Worker
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching tactical assets...');
-      cache.addAll(assets);
-    })
-  );
-});
-
-// Fetch Assets
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(rec => {
-      return rec || fetch(evt.request);
-    })
-  );
-});
