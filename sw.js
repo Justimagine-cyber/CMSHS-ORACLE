ac1841947fcb950093d4d7dd517ce1b96b0e9ba9
Justimagine-cyber <83823401+Justimagine-cyber@users.noreply.github.com>
2026-03-12 20:29:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oracle-cmshs-v17.143';
+const CACHE_NAME = 'oracle-cmshs-v17.144';
 const urlsToCache = [
   '/',
   'index.html',
@@ -46,6 +46,7 @@ self.addEventListener('fetch', evt => {
     })
   );
 });
+
 
 
 
